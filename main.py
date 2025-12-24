894d80aeac1b432cfcd232d016f41cb1e6187cd8
wolerood <65359388+wolerood@users.noreply.github.com>
2025-12-24 17:25:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ import threading
 import sweetie_web
 import sw_detect
 
+'''
 threading.Thread(
     target=sweetie_web.run_flask,
     daemon=True
 ).start()
 
 sw_detect.detection_loop()
+'''
+
+
+# запуск детекции в фоне
+threading.Thread(
+    target=sw_detect.detection_loop,
+    daemon=True
+).start()
+
+# Flask — ТОЛЬКО в главном потоке
+sweetie_web.run_flask()
