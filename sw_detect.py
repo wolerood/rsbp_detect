ebc8afa60f75681b3fb4f5fb2186ee5be7259b7a
wolerood <65359388+wolerood@users.noreply.github.com>
2026-04-08 16:57:51 +0300

    Update sw_detect.py

--- sw_detect.py
+++ sw_detect.py
@@ -24,7 +24,7 @@ def detection_loop():
     time.sleep(2)  # задержка перед стартом
 
     # ===== загрузка модели =====
-    model = YOLO("yolov8n.pt")
+    model = YOLO("yolov12n.pt")
 
     # ===== счётчики FPS =====
     frame_count = 0
