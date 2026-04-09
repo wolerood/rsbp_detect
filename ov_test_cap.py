2e882fe3131eecfa166d9a9b50d33afee7fe76cd
wolerood <65359388+wolerood@users.noreply.github.com>
2026-04-09 17:40:51 +0300

    Update ov_test_cap.py

--- ov_test_cap.py
+++ ov_test_cap.py
@@ -2,9 +2,14 @@ from ultralytics import YOLO
 import cv2
 import time
 
+model = YOLO("yolo26n.pt")
+model.export(format="openvino", imgsz=320, half=False)
+
+'''
 model = YOLO("yolo26n_openvino_model")
 img = cv2.imread("test.jpg")
 
 t0 = time.time()
 results = model(img, imgsz=320, verbose=False)
 print("Inference time:", time.time() - t0)
+'''
