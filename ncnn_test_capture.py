609c87d28b94efa18517e0b09e8e14c8fba57060
wolerood <65359388+wolerood@users.noreply.github.com>
2026-04-09 14:47:33 +0300

    Update ncnn_test_capture.py

--- ncnn_test_capture.py
+++ ncnn_test_capture.py
@@ -1,7 +1,23 @@
 import cv2
-from ultralytics import YOLO
 
-img = cv2.imread("bus.jpg")
-model = YOLO("yolo26n_ncnn_model", task="detect")
-res = model(img, imgsz=320) #, verbose=False)
-print("inference ok", len(res))
+cap = cv2.VideoCapture("/dev/video8", cv2.CAP_V4L2)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FPS, 30)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+
+if not cap.isOpened():
+    raise RuntimeError("Не удалось открыть /dev/video8")
+
+ret, frame = cap.read()
+cap.release()
+
+if not ret:
+    raise RuntimeError("Не удалось получить кадр")
+
+print("shape:", frame.shape)
+print("dtype:", frame.dtype)
+print("contiguous:", frame.flags["C_CONTIGUOUS"])
+
+cv2.imwrite("test_frame.jpg", frame)
+print("saved: test_frame.jpg")
