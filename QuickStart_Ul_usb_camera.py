ac94bbafc1517ae93294750fb1f81428b744fb86
wolerood <65359388+wolerood@users.noreply.github.com>
2026-04-09 12:34:00 +0300

    add

---
+++ QuickStart_Ul_usb_camera.py
@@ -0,0 +1,44 @@
+import cv2
+from ultralytics import YOLO
+import time
+
+cap = cv2.VideoCapture("/dev/video8", cv2.CAP_V4L2)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FPS, 30)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+
+if not cap.isOpened():
+    raise RuntimeError("Не удалось открыть /dev/video8")
+
+frame_count = 0
+start_time = time.time()
+
+model = YOLO("yolo26n.pt")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        continue
+
+    frame = cv2.flip(frame, 0)
+    results = model(frame, imgsz=320, verbose=False)
+    annotated_frame = results[0].plot()
+
+    #FPS count
+    frame_count += 1
+    elapsed_time = time.time() - start_time
+    fps = frame_count / elapsed_time if elapsed_time > 0 else 0
+
+    cv2.putText(annotated_frame, f"FPS: {fps:.2f}",
+                (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+                0.7, (0, 255, 255), 2)
+
+    cv2.imshow("USB Camera YOLO", annotated_frame)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
