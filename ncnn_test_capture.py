5da4b08c99d7659f001391b18788f93d83756136
wolerood <65359388+wolerood@users.noreply.github.com>
2026-04-09 14:44:26 +0300

    Update ncnn_test_capture.py

--- ncnn_test_capture.py
+++ ncnn_test_capture.py
@@ -1,7 +1,7 @@
 import cv2
 from ultralytics import YOLO
 
-img = cv2.imread("https://ultralytics.com/images/bus.jpg")
+img = cv2.imread("bus.jpg")
 model = YOLO("yolo26n_ncnn_model", task="detect")
 res = model(img, imgsz=320) #, verbose=False)
 print("inference ok", len(res))
