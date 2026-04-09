b888c99c3a52d83241eb1892033a07fa73b1da59
wolerood <65359388+wolerood@users.noreply.github.com>
2026-04-09 09:15:58 +0300

    ad

--- main.py
+++ main.py
@@ -2,16 +2,6 @@ import threading
 import sweetie_web
 import sw_detect
 
-'''
-threading.Thread(
-    target=sweetie_web.run_flask,
-    daemon=True
-).start()
-
-sw_detect.detection_loop()
-'''
-
-
 # запуск детекции в фоне
 threading.Thread(
     target=sw_detect.detection_loop,
