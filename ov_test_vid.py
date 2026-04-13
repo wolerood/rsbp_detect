635009bfb13114fa9c110fd7212044483119379c
wolerood <65359388+wolerood@users.noreply.github.com>
2026-04-13 13:43:35 +0300

    Update ov_test_vid.py

--- ov_test_vid.py
+++ ov_test_vid.py
@@ -2,7 +2,7 @@ import cv2
 import time
 from ultralytics import YOLO
 
-cap = cv2.VideoCapture("/dev/video8", cv2.CAP_V4L2)
+cap = cv2.VideoCapture("/dev/video0", cv2.CAP_V4L2)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 cap.set(cv2.CAP_PROP_FPS, 30)
